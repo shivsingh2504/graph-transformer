cc7a445c14f11bb1169b822408b205b7fac90184
shivsingh2504 <shivsinghs2504@gmail.com>
2026-09-15 01:22:32 +0530

    Added all the hyper parameters in train.py

--- src/train/train.py
+++ src/train/train.py
@@ -12,3 +12,14 @@ from data.dataset_generator import DatasetSplit
 from data.tokenizer import GraphTokenizer
 from model.layers import create_causal_mask,create_padding_mask
 from model.model import Transformer
+
+
+_D_MODEL : int = 128
+_N_HEADS : int = 4
+_N_LAYERS: int = 3
+_D_FF:int = 512
+_DROPOUT : float = 0.1
+_LR: float = 1e-4
+_WEIGHT_DECAY : float = 1e-2
+_WARMUP_STEPS:int = 400
+_BATCH_SIZE: int = 32
